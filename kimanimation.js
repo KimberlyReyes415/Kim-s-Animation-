38a47907c4b60b3ca8da6389d9fe46d1fb1fd4f9
Kimberly C Reyes <kr61336@srcs.loc>
2018-06-05 12:19:10 -0700

    edit

--- kimanimation.js
+++ kimanimation.js
@@ -19,45 +19,64 @@ function moveThing(event) {
     thing.style.top = yPosition + "px";
 }
 
-function Box(xpos, ypos, xstep, ystep, id) {
-    this.xpos = xpos;
-    this.ypos = ypos;
-    this.xstep = xstep;
-    this.ystep = ystep;
-    this.id = id;
-    var elem = document.getElementById(this.id);
-    this.render = function () {
-        elem.style.top = this.ypos + 'px';
-        elem.style.left = this.xpos + 'px';
-    }
-}
 
-var box1 = new Box(0, 0, 1, 2, "animation1");
-var box2 = new Box(100, 0, 5, 5, "animation2");
+var box1 = document.getElementById("animation1");
+var xpos1 = 0;
+var ypos1 = 0;
+var xstep1 = 2;
+var ystep1 = 1;
 
-var boxes = [box1, box2];
+var box2 = document.getElementById("animation2");
+var xpos2 = 0;
+var ypos2 = 500;
+var xstep2 = 3;
+var ystep2 = 2;
 
-var id = setInterval(frame, 20);
+var box3 = document.getElementById("animation3");
+var xpos3 = 0;
+var ypos3 = 200;
+var xstep3 = 3;
+var ystep3 = 2;
+var id = setInterval(frame, 25);
+
+
+
 
 function frame() {
 
-    for (var i = 0; i < boxes.length; i++) {
-        if (boxes[i].xpos > 350 || boxes[i].xpos < 0) {
-            boxes[i].xstep = -boxes[i].xstep;
-        }
-        if (boxes[i].ypos > 350 || boxes[i].ypos < 0) {
-            boxes[i].ystep = -boxes[i].ystep;
-        }
-
-        boxes[i].xpos = boxes[i].xpos + boxes[i].xstep;
-        boxes[i].ypos = boxes[i].ypos + boxes[i].ystep;
-        boxes[i].render();
+    if (xpos1 > 350 || xpos1 < 0) {
+        xstep1 = -xstep1;
     }
-}
+
+
+    if (xpos2 > 350 || xpos2 < 0) {
+        xstep2 = -xstep2;
+    }
+    if (ypos2 > 600 || ypos2 < 0) {
+        ystep2 = -ystep2;
+    }
+
+    if (xpos3 > 350 || xpos3 < 0) {
+        xstep3 = -xstep3;
+    }
+    if (ypos3 > 350 || ypos3 < 0) {
+        ystep3 = -ystep3;
+    }
+
     xpos1 = xpos1 + xstep1;
-    ypos1 = ypos1 + ystep1;
-    box1.style.top = ypos1 + 'px';
     box1.style.left = xpos1 + 'px';
+
+
+    xpos2 = xpos2 + xstep2;
+    ypos2 = ypos2 + ystep2;
+    box2.style.top = ypos2 + 'px';
+    box2.style.left = xpos2 + 'px';
+
+    xpos3 = xpos3 + xstep3;
+    ypos3 = ypos3 + ystep3;
+    box3.style.top = ypos3 + 'px';
+    box3.style.left = xpos3 + 'px';
+}
 
 window.addEventListener("keydown", function (event) {
     if (event.key === "ArrowDown") {
